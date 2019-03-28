6163369084dce1277cf26e23002fda8d64b8a881
HolaPI <shevarwin@126.com>
2019-03-28 19:22:41 +0800

    achieved add task and delete task

--- ToDoApp/controller/todoController.js
+++ ToDoApp/controller/todoController.js
@@ -1,12 +1,27 @@
+var bodyParser = require('body-parser')
+var urlencodeParser = bodyParser.urlencoded({ extended: false })
+var data = [
+    { item: 'welcome to be here' },
+    { item: 'have you finished your work today' },
+    { item: 'good to see you here' },
+    { item: 'you readlly did a great job today' }
+]
 
 module.exports = function (app) {
+    //get data
     app.get('/todo', (req, res) => {
-        res.render('todo')
+        res.render('todo', { todos: data })
     })
-    app.post('./todo', (req, res) => {
-
+    //transit data
+    app.post('/todo', urlencodeParser, (req, res) => {
+        data.push(req.body);
     })
-    app.delete('./todo', (req, res) => {
-
+    //delete data
+    app.delete('/todo:context', (req, res) => {
+        // console.log(req.params.context)
+        data = data.filter(function (item) {
+            return item.item !== req.params.context;
+        });
+        res.json(data);
     })
 }
